c75bf65fb09e6a2d0f58d88b97072d2a1612afa8
Calum O'Donnell <calumodonnell@supanet.com>
2016-09-16 11:05:42 +0100

    1.1.6
    - cart updated to handle total price of cables
    - optimised code
    - max/min values detected

--- js/app.js
+++ js/app.js
@@ -371,10 +371,6 @@ app.controller('connectorCtrl', function ($scope, $http, $location) {
     };
 
     /*
-    $scope.handleDrop_1 = function (item, conn) {
-
-    };
-
     $scope.handleDrop_2 = function (item, conn) {
         localStorage.setItem(conn, item);
         $scope.conn_2 = item;
@@ -549,9 +545,6 @@ app.controller('connectorCtrl', function ($scope, $http, $location) {
         localStorage.setItem('conn_2', '');
     };
 
-
-
-
     $scope.centerAnchor = true;
     $scope.toggleCenterAnchor = function () { $scope.centerAnchor = !$scope.centerAnchor; };
     $scope.draggableObjects = [{name: 'subject1'}, {name: 'subject2'}, {name: 'subject3'}];
@@ -592,67 +585,8 @@ app.controller('connectorCtrl', function ($scope, $http, $location) {
     };
 });
 
-/*
 
-var drag = angular.module('drag', ['ngDraggable']);
-
-app.controller('controller', function($scope) {
-    $scope.listItems = [{
-        name: "some name",
-        title: "title1"
-    }, {
-        name: "some name2",
-        title: "title2"
-    }, {
-        name: "some name3",
-        title: "title3"
-    }, ];
-
-    $scope.droppedObjects = [];
-    $scope.input = {};
-
-    $scope.onDragComplete = function(data, evt) {
-        console.log("drag success, data:", data);
-        var index = $scope.droppedObjects.indexOf(data);
-        if (index > -1) {
-            $scope.droppedObjects.splice(index, 1);
-        }
-    };
-
-    $scope.onDropComplete = function(data, evt) {
-        console.log("drop success, data:", data);
-        var index = $scope.droppedObjects.indexOf(data);
-        if (index == -1) {
-            $scope.droppedObjects.push(data);
-        }
-    };
-
-    $scope.onDropCompleteInput = function(data, evt) {
-        console.log("drop on input success, data:", data);
-        $scope.input = data;
-    };
-
-    $scope.onDropCompleteRemove = function(data, evt) {
-        console.log("drop success - remove, data:", data);
-        var index = $scope.droppedObjects.indexOf(data);
-        if (index != -1) {
-            $scope.droppedObjects.splice(index);
-        }
-    };
-
-    var onDraggableEvent = function(evt, data) {
-        console.log("128", "onDraggableEvent", evt, data);
-    };
-
-    $scope.$on('draggable:start', onDraggableEvent);
-    //$scope.$on('draggable:move', onDraggableEvent);
-    $scope.$on('draggable:end', onDraggableEvent);
-});
-*/
-
-
-
-app.controller('cartCtrl', function ($scope) {
+app.controller('cartCtrl', function ($scope, $log) {
     "use strict";
 
     $scope.cart = JSON.parse(localStorage.getItem('cart'));
@@ -747,6 +681,24 @@ app.controller('cartCtrl', function ($scope) {
         return total;
     };
 
+
+    $scope.totalPrice = function () {
+        var total = 0,
+            i,
+            product,
+            cart;
+
+        localStorage.cart = localStorage.getItem('cart');
+        cart = JSON.parse(localStorage.cart);
+
+        for (i = 0; i < cart.length; i += 1) {
+            total = total + cart[i].price;
+        }
+
+        return total;
+    };
+
+
     $scope.errorHide = function () {
         $scope.err = false;
     };
@@ -794,16 +746,6 @@ app.controller('cartCtrl', function ($scope) {
         localStorage.cart = localStorage.getItem('cart');
         var cart = JSON.parse(localStorage.cart);
 
-        if (val > 3024 && $scope.metric === true) {
-            $scope.err = true;
-            $scope.error_message = "This program has a maxiumum length of 3024 cm. Please contact the factory for custom lengths.";
-            $scope.clength = 3024;
-        } else if (val > 1200 && $scope.metric === false) {
-            $scope.err = true;
-            $scope.error_message = "This program has a maxiumum length of 1200 in. Please contact the factory for custom lengths.";
-            $scope.clength = 1200;
-        }
-
         if (val === null) {
             val = 6;
             cart[index].length = val;
@@ -814,18 +756,29 @@ app.controller('cartCtrl', function ($scope) {
         localStorage.cart = JSON.stringify(cart);
     };
 
-    $scope.totalPrice = function () {
-        var total = 0,
-            i,
-            product;
 
-        for (i = 0; i < $scope.cart.length; i += 1) {
-            product = $scope.cart[i];
-            total = total + product.price;
+    $scope.lengthCheck = function (len, index) {
+        if (len < 15 && $scope.metric === true) {
+            $scope.err = true;
+            $scope.error_message = "This program has a minimum length of 15 cm. Please contact the factory for custom lengths.";
+        } else if (len < 6 && $scope.metric === false) {
+            $scope.err = true;
+            $scope.error_message = "This program has a maxiumum length of 6 in. Please contact the factory for custom lengths.";
+        } else if (len > 3024 && $scope.metric === true) {
+            $scope.err = true;
+            $scope.error_message = "This program has a minimum length of 15 cm. Please contact the factory for custom lengths.";
+        } else if (len > 1200 && $scope.metric === false) {
+            $scope.err = true;
+            $scope.error_message = "This program has a maxiumum length of 6 in. Please contact the factory for custom lengths.";
         }
-
-        return total;
     };
+
+
+    if (localStorage.getItem('measure') === 'true') {
+        $scope.metric = true;
+    } else {
+        $scope.metric = false;
+    }
 
     // update length depending on measurement type
     $scope.$watch('metric', function () {
@@ -844,14 +797,16 @@ app.controller('cartCtrl', function ($scope) {
                 }
                 len = $scope.clength.toFixed(0);
                 $scope.clength = parseInt(len, 10);
+
+                localStorage.setItem('clength', $scope.clength);
             }
         }
     });
 
+
     $scope.printDiv = function () {
         var pdf = new jsPDF('p', 'pt', 'a4'),
             source = "<html> <head> <title>Cable Assembly Cart Summary</title> <style type='text/css'> .pdf-container{font-family: -apple-system,BlinkMacSystemFont,'Segoe UI',Roboto,'Helvetica Neue',Arial,sans-serif; color: #58595B; font-weight: 300; font-size: 12px;}.pdf-header{margin-bottom: 20px;}.address-header{display: inline-block;}p{margin: 0; margin-bottom: 5px;}b{font-weight: 600}.logo-header{display: inline-block; vertical-align: top; float: right;}.logo-header img{width: 80px; height: auto;}.pdf-content{margin-bottom: 20px;}table{width: 100%;}td, th{padding: 4px 8px}tbody tr:nth-child(even){background-color: #F2F2F2;}.pdf-footer{clear: both;}.pdf-footer .sales-disclaimer{display: inline-block;}.pdf-footer .printed-date{display: inline-block;}</style> </head> <body> <div class='pdf-container'> <header class='pdf-header'> <div class='address-header'> <p><b>Florida RF Labs, Inc.</b></p><p>8851 SW Old Kansas Ave.</p><p>Stuart, FL 34997</p><p>Tel: (772) 286-9300</p><p>Fax: (772) 283-5286</p><p><a href='http://www.emc-rflabs.com'>www.emc-rflabs.com</a></p></div><div class='logo-header'> <img src='./wp-content/plugins/cable-wizard/img/rflabs.png'> </div></header> <div class='pdf-content'> <h1>Cable Assembly Cart Summary</h1> <table> <thead> <tr> <th>Order Number</th> <th>Part Number</th> <th>Quantity</th> <th>Unit Price</th> <th>Ext. Amount</th> </tr></thead> <tbody> <tr> <td>AL141LLSPS1S1#00060</td><td>SMS-AL141LLSP-6.0-SMS</td><td>1</td><td>$124.49</td><td>$124.49</td></tr><tr> <td>Price Breaks</td><td colspan='4'> <table> <tr> <td>1 - 3 <br>$124.49</td><td>4 - 9 <br>$100.58</td><td>10 - 24 <br>$78.68</td><td>25 - 49 <br>$68.55</td><td>50 - 99 <br>$60.67</td><td>100+ <br>$52.78</td></tr></table> </td></tr><tr> <td>085S1S2#00060</td><td>SMS-085-6.0-SMR</td><td>1</td><td>$85.33</td><td>$85.33</td></tr><tr> <td>Price Breaks</td><td colspan='4'> <table> <tr> <td>1 - 3 <br>$85.33</td><td>4 - 9 <br>$68.49</td><td>10 - 24 <br>$51.65</td><td>25 - 49 <br>$45.92</td><td>50 - 99 <br>$40.37</td><td>100+ <br>$34.81</td></tr></table> </td></tr></tbody> <tfoot> <tr> <td></td><td></td><td></td><td></td><td>Total: $209.82</td></tr></tfoot> </table> </div><footer class='pdf-footer'> <div class='sales-disclaimer'> <p>THIS QUOTE IS SUBJECT TO ALL EXPORT CONTROL REGULATIONS OF THE UNITED STATES.</p><p>Cable Assemblies have a $250.00 line item minimum requirement.</p><p>Component Product line has a $1000.00 line item minimum requirement.</p><p>Lead times quoted are ARO (after receipt of order) and does not include transit time.</p><p>Prices are based on the information available at the time of quotation. Quality Assurance provisions, First Article Verification, Source Inspection and Special Packaging requirements not quoted and appear on the purchase order may affect prices quoted herein. Florida RF Labs reserves the right to amend this quotation if these requirements are not quoted and appear on the purchase order.</p><p>Click <a href='http://www.emc-rflabs.com/Rflabs/media/Generic-Library/General%20Information/432F024-EMC-RF-LABS-SALES-TERMS-AND-CONDITIONS.pdf' target='_blank'>here</a> for T&Cs</p></div><div class='printed-date'> <p>PRINTED: <span id='currentdate'>09/14/2016</span></p></div></footer> </div></body></html>",
-            //source = jQuery('.print-preview')[0],
             margins = {
                 top: 40,
                 bottom: 60,
@@ -876,7 +831,6 @@ app.controller('cartCtrl', function ($scope) {
     $scope.printDrawing = function () {
         var pdf = new jsPDF('p', 'pt', 'a4'),
             source = "<html> <head> <title>Cable Assembly Cart Summary</title> <style type='text/css'> .pdf-container{font-family: -apple-system,BlinkMacSystemFont,'Segoe UI',Roboto,'Helvetica Neue',Arial,sans-serif; color: #58595B; font-weight: 300; font-size: 12px;}.pdf-header{margin-bottom: 20px;}.address-header{display: inline-block;}p{margin: 0; margin-bottom: 5px;}b{font-weight: 600}.logo-header{display: inline-block; vertical-align: top; float: right;}.logo-header img{width: 80px; height: auto;}.pdf-content{margin-bottom: 20px;}table{width: 100%;}td, th{padding: 4px 8px}tbody tr:nth-child(even){background-color: #F2F2F2;}.pdf-footer{clear: both;}.pdf-footer .sales-disclaimer{display: inline-block;}.pdf-footer .printed-date{display: inline-block;}</style> </head> <body> <div class='pdf-container'> <header class='pdf-header'> <div class='address-header'> <p><b>Florida RF Labs, Inc.</b></p><p>8851 SW Old Kansas Ave.</p><p>Stuart, FL 34997</p><p>Tel: (772) 286-9300</p><p>Fax: (772) 283-5286</p><p><a href='http://www.emc-rflabs.com'>www.emc-rflabs.com</a></p></div><div class='logo-header'> <img src='./wp-content/plugins/cable-wizard/img/rflabs.png'> </div></header> <div class='pdf-content'> <h1>Cable Assembly Cart Summary</h1> <table> <thead> <tr> <th>Order Number</th> <th>Part Number</th> <th>Quantity</th> <th>Unit Price</th> <th>Ext. Amount</th> </tr></thead> <tbody> <tr> <td>AL141LLSPS1S1#00060</td><td>SMS-AL141LLSP-6.0-SMS</td><td>1</td><td>$124.49</td><td>$124.49</td></tr><tr> <td>Price Breaks</td><td colspan='4'> <table> <tr> <td>1 - 3 <br>$124.49</td><td>4 - 9 <br>$100.58</td><td>10 - 24 <br>$78.68</td><td>25 - 49 <br>$68.55</td><td>50 - 99 <br>$60.67</td><td>100+ <br>$52.78</td></tr></table> </td></tr><tr> <td>085S1S2#00060</td><td>SMS-085-6.0-SMR</td><td>1</td><td>$85.33</td><td>$85.33</td></tr><tr> <td>Price Breaks</td><td colspan='4'> <table> <tr> <td>1 - 3 <br>$85.33</td><td>4 - 9 <br>$68.49</td><td>10 - 24 <br>$51.65</td><td>25 - 49 <br>$45.92</td><td>50 - 99 <br>$40.37</td><td>100+ <br>$34.81</td></tr></table> </td></tr></tbody> <tfoot> <tr> <td></td><td></td><td></td><td></td><td>Total: $209.82</td></tr></tfoot> </table> </div><footer class='pdf-footer'> <div class='sales-disclaimer'> <p>THIS QUOTE IS SUBJECT TO ALL EXPORT CONTROL REGULATIONS OF THE UNITED STATES.</p><p>Cable Assemblies have a $250.00 line item minimum requirement.</p><p>Component Product line has a $1000.00 line item minimum requirement.</p><p>Lead times quoted are ARO (after receipt of order) and does not include transit time.</p><p>Prices are based on the information available at the time of quotation. Quality Assurance provisions, First Article Verification, Source Inspection and Special Packaging requirements not quoted and appear on the purchase order may affect prices quoted herein. Florida RF Labs reserves the right to amend this quotation if these requirements are not quoted and appear on the purchase order.</p><p>Click <a href='http://www.emc-rflabs.com/Rflabs/media/Generic-Library/General%20Information/432F024-EMC-RF-LABS-SALES-TERMS-AND-CONDITIONS.pdf' target='_blank'>here</a> for T&Cs</p></div><div class='printed-date'> <p>PRINTED: <span id='currentdate'>09/14/2016</span></p></div></footer> </div></body></html>",
-            //source = jQuery('.print-preview')[0],
             margins = {
                 top: 40,
                 bottom: 60,
